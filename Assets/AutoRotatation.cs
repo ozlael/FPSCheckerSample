d0d94cd0dc28718e9d192ffadbd9c63e03c7e922
ozlael <ozjjangozjjang@gmail.com>
2017-12-31 20:18:11 +0900

    Initial commit

---
+++ Assets/AutoRotatation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotatation : MonoBehaviour {
+
+	[SerializeField]
+	float speed = 0;
+
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up * speed * Time.deltaTime);
+	}
+}
---
+++ Assets/DisplayFPS.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayFPS : MonoBehaviour {
+
+	[SerializeField]
+	Text text;
+
+	float frames = 0f;
+	float timeElap = 0f;
+
+	void Start() {
+	}
+
+	void Update() {
+		frames++;
+		timeElap += Time.unscaledDeltaTime;
+		if (timeElap > 1f) {
+			timeElap -= 1f;
+			UpdateText ();
+			frames =0;
+		}
+	}
+
+	void UpdateText() {
+		text.text = string.Format (
+			"FPS : {0}, FrameTime : {1:F2} ms",
+			frames, Time.unscaledDeltaTime * 1000.0f);
+	}
+}
